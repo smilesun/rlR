7bb50d14df4d49156c0b4793af5155531fd5d61e
Markus Dumke <markusdumke@gmail.com>
2017-12-04 10:35:05 +0100

    add class for representation of value function with predict and train function

--- R/valuefunction.R
+++ R/valuefunction.R
@@ -1,17 +1,41 @@
 # ActionValueNetwork
+# keras or mxnet
+ActionValueNetwork = R6::R6Class("ActionValueNetwork",
+  public = list(
+    model = NULL,
+    preprocessState = NULL,
+    # keras or mxnet model
+    initialize = function(model, preprocessState) {
+      self$model = model
+      self$preprocessState = preprocessState
+    },
+    predictQ = function(state) {
+      state = self$preprocessState(state)
+      predict(self$model, state) # another function?
+    },
+    train = function(state, target) {
+      state = self$preprocessState(state)
+      keras::fit(self$model, state, target, verbose = 0)
+    }
+  )
+)
 
 # ActionValueTable
 ActionValueTable = R6::R6Class("ActionValueTable",
   public = list(
     Q = NULL,
-    # get number of states and actions automatically from environment
-    # custom initializer, e.g. not to 0
-    initialize = function(n.states, n.actions) {
+    step.size = NULL,
+    # fixme: get number of states and actions automatically from environment
+    # fixme: custom initializer, e.g. not to 0
+    initialize = function(n.states, n.actions, step.size) {
       self$Q = matrix(0, nrow = n.states, ncol = n.actions)
-      print("Not implemented")
+      self$step.size = step.size
     },
-    predict = function(state) {
-      Q[state + 1, ]
+    predictQ = function(state) {
+      self$Q[state + 1, ]
+    },
+    train = function(state, target) {
+      self$Q[state + 1, ] = self$Q[state + 1, ] + self$step.size * (target - self$Q[state + 1, ])
     }
   )
 )
