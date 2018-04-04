84bf1990c9dcacb804b47b8430b0e8b3cb0cef59
smilesun <smilesun.east@gmail.com>
2018-04-04 21:02:40 +0200

    .

---
+++ tests/testthat/test_gym_basic.R
@@ -0,0 +1,37 @@
+makeDefaultConfig = function(scenario = "CartPole-v0" ) {
+  conf = RLConf$new()
+  conf$updatePara("gym", "scenarioname", scenario)
+  conf$updatePara("interact", "maxiter", 2L)
+  conf$updatePara("agent", "EPSILON", 0.01)
+  conf$updatePara("agent", "agentname", "DQN")
+  conf$updatePara("agent", "replayBatchSize", 5L)
+  conf$updatePara("agent", "memname", "latest")
+  conf$updatePara("agent", "policy", "epsilonGreedy")
+  conf$updatePara("nn", "archname", "mountaincar-linear-noreg")
+  return(conf)
+}
+
+# ensure this test case work every time code  is changed
+regressionTest_Mountaincar = function() {  
+  conf = makeDefaultConfig("MountainCar-v0")
+  interact = makeGymExperiment(conf = conf) 
+  perf = interact$run()
+  return(perf)
+} 
+
+regressionTest_CartPole = function() {  
+  conf = makeDefaultConfig("CartPole-v0")
+  conf$updatePara("agent", "agentname", "A3C")
+  interact = makeGymExperiment(conf = conf) 
+  perf = interact$run()
+  return(perf)
+}
+context("gym_basic")
+test_that("test MountainCar works", {
+  regressionTest_Mountaincar()
+})
+test_that("test Cart-Pole works", {
+  regressionTest_CartPole()
+})
+
+ 
