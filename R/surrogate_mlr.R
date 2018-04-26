927840d274019d04357b215138424fb4a33cee98
smilesun <smilesun.east@gmail.com>
2018-04-26 15:12:29 +0200

    .

--- R/surrogate_mlr.R
+++ R/surrogate_mlr.R
@@ -8,18 +8,21 @@ Surro.mlr = R6Class("Surro.mlr",
     },
 
     train = function(X_train, Y_train) {
-      if(nrow(X_train) < 100) 
+      if (nrow(X_train) < 50) 
         return(NULL)
       lrn = mlr::makeLearner("regr.ranger")
       df = as.data.frame(cbind(Y_train, X_train))
-      colnames(df)[1] = "rlr.target"
+      colnames(df)[1] = c("rlr.target")
+      df[2: self$actCnt] = NULL
       task = mlr::makeRegrTask(data = df, target = "rlr.target")
+      browser()
       self$model = mlr::train(lrn, task)
     },
 
     pred = function(X) {
       n = nrow(X)
-      if (is.null(self$model)) return(matrix(c(rep(1,n), rep(0,n)), nrow = n))
+      h = rnorm(n, mean = 0.5, sd = 0.1)
+      if (is.null(self$model)) return(matrix(c(h, 1-h), nrow = n))
       preds = predict(self$model, newdata = X)
       cbind(preds, 1 - preds)
     }
