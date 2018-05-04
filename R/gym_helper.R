1a451cf3f8d7d338b4f077c5e88960b1daa2257c
smilesun <smilesun.east@gmail.com>
2018-05-04 17:50:33 +0200

    .

--- R/gym_helper.R
+++ R/gym_helper.R
@@ -2,12 +2,12 @@ dqn_mountain_car = function(iter = 1L) {
   conf = rlR::RLConf$new(
            agent.name = "AgentDQN",
            policy.epsilon = 1,
-           policy.decay = exp(-0.001),
+           policy.decay = exp(-0.2),
            policy.name = "policy.epsilonGreedy",
            replay.memname = "Uniform",
            replay.batchsize = 60L,
            interact.beforeActPipe = c("render", "epi-step-log"),
-           agent.nn.arch = list(nhidden = 64, act1 = "relu", act2 = "linear", loss = "mse", lr = 0.00025, kernel_regularizer = "regularizer_l2(l=0)", bias_regularizer = "regularizer_l2(l=0)")
+           agent.nn.arch = list(nhidden = 64, act1 = "sigmoid", act2 = "linear", loss = "mse", lr = 0.00025, kernel_regularizer = "regularizer_l2(l=0)", bias_regularizer = "regularizer_l2(l=0)")
            )
   interact = rlR::makeGymExperiment(name = "MountainCar-v0", conf = conf, actcnt = 2L, act.cheat = function(x) {
   ifelse(x == 1L, 0L, 2L)  # we do not need the stay action, this is a way to cheat but it makes learning much faster.
@@ -20,10 +20,10 @@ ddqn_mountain_car = function(iter = 1L) {
   conf = rlR::RLConf$new(
            agent.name = "AgentDDQN",
            policy.epsilon = 1,
-           policy.decay = exp(-0.05),
+           policy.decay = exp(-0.2),
            policy.name = "policy.epsilonGreedy",
            replay.batchsize = 50L,
-           agent.nn.arch = list(nhidden = 64, act1 = "linear", act2 = "linear", loss = "mse", lr = 0.00005, kernel_regularizer = "regularizer_l2(l=0.000001)", bias_regularizer = "regularizer_l2(l=0.000011)"))
+           agent.nn.arch = list(nhidden = 64, act1 = "sigmoid", act2 = "linear", loss = "mse", lr = 0.00005, kernel_regularizer = "regularizer_l2(l=0.000001)", bias_regularizer = "regularizer_l2(l=0.000011)"))
   interact = rlR::makeGymExperiment(name = "MountainCar-v0", conf = conf)
   perf = interact$run(iter)
   return(perf)
@@ -125,7 +125,7 @@ pg = function(iter = 1L, name = "MountainCar-v0") {
            policy.minEpsilon = 0.001,
            replay.memname = "Latest",
            replay.epochs = 1L,
-           agent.nn.arch = list(nhidden = 64, act1 = "relu", act2 = "softmax", loss = "categorical_crossentropy", lr = 5e-6, kernel_regularizer = "regularizer_l2(l=0.0)", bias_regularizer = "regularizer_l2(l=0)"))
+           agent.nn.arch = list(nhidden = 64, act1 = "sigmoid", act2 = "softmax", loss = "categorical_crossentropy", lr = 5e-6, kernel_regularizer = "regularizer_l2(l=0.0)", bias_regularizer = "regularizer_l2(l=0)"))
   interact = rlR::makeGymExperiment(name = name, conf = conf)
   perf = interact$run(iter)
   return(perf)
@@ -183,8 +183,8 @@ mlr_mountain_car = function(iter = 1L) {
            policy.epsilon = 1,
            policy.decay = exp(-0.01),
            policy.name = "policy.predsoftmax",
-           replay.batchsize = 50L,
-           agent.nn.arch = list(nhidden = 64, act1 = "relu", act2 = "linear", loss = "mse", lr = 0.00005, kernel_regularizer = "regularizer_l2(l=0.000001)", bias_regularizer = "regularizer_l2(l=0.000011)"))
+           replay.batchsize = 50L
+           )
   interact = rlR::makeGymExperiment(name = "MountainCar-v0", conf = conf)
   perf = interact$run(iter)
   return(perf)
@@ -197,8 +197,8 @@ mlr_cartepole = function(iter = 1L) {
            policy.epsilon = 1,
            policy.decay = exp(-0.01),
            policy.name = "policy.predsoftmax",
-           replay.batchsize = 50L,
-           agent.nn.arch = list(nhidden = 64, act1 = "relu", act2 = "linear", loss = "mse", lr = 0.00005, kernel_regularizer = "regularizer_l2(l=0.000001)", bias_regularizer = "regularizer_l2(l=0.000011)"))
+           replay.batchsize = 50L
+           )
   interact = rlR::makeGymExperiment(name = "CartPole-v0", conf = conf)
   perf = interact$run(iter)
   return(perf)
