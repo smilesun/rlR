d044db0133b2dc78de44de5c1dbc494150141a08
smilesun <smilesun.east@gmail.com>
2018-05-30 20:45:16 +0200

    .

--- R/agent_fdqn.R
+++ R/agent_fdqn.R
@@ -47,16 +47,6 @@ AgentFDQN = R6::R6Class("AgentFDQN",
   )
 
 
-conf = RLConf$new(
-           render = render,
-           policy.maxEpsilon = 1,
-           policy.minEpsilon = 0.01,
-           policy.decay = exp(-0.001),
-           policy.name = "EpsilonGreedy",
-           replay.batchsize = 64L,
-           agent.nn.arch = list(nhidden = 64, act1 = "relu", act2 = "linear", loss = "mse", lr = 0.00005, kernel_regularizer = "regularizer_l2(l=0.000001)", bias_regularizer = "regularizer_l2(l=0.000011)"))
-
-
 AgentFDQN$test = function(iter = 500L, sname = "CartPole-v0", render = TRUE, console = FALSE) {
   conf = RLConf$new(
            render = render,
