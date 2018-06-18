b9963ff305ade81b7ef0ec0e4cc9797b6ed0c612
smilesun <smilesun.east@gmail.com>
2018-06-18 18:35:58 +0200

    .

--- benchmark/bt_algorithms.R
+++ benchmark/bt_algorithms.R
@@ -1,6 +1,6 @@
 rl_algo = function(data, job, instance, agent.name) {
   env = makeGymEnv(name = instance)
-  makeAgent(agent.name, env = env)
+  agent = makeAgent(agent.name, env = env)
   perf = agent$learn(data$iteration)
   return(perf = perf)  # key for table join
 }
