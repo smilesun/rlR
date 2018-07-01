5d7f5a894042dd065ee39be7cf403ca71f7d5680
smilesun <smilesun.east@gmail.com>
2018-07-02 01:01:48 +0200

    .

--- R/agent_fdqn.R
+++ R/agent_fdqn.R
@@ -78,7 +78,7 @@ AgentFDQN$test = function(iter = 1000L, sname = "CartPole-v0", render = FALSE, c
 AgentFDQN$testCnn = function(iter = 5000, render = TRUE) {
   conf = getDefaultConf("AgentFDQN")
   #@Note: one episode of pong is around 300 steps
-  conf$set(replay.batchsize = 32, replay.freq = 4L, console = TRUE, agent.lr.decay = 1, agent.lr = 0.00025, replay.memname = "UniformStack", render = render, policy.decay = exp(-2.2 / 1e6), policy.minEpsilon = 0.1, agent.start.learn = 500L, replay.mem.size = 1e6, log = TRUE)
+  conf$set(replay.batchsize = 32, replay.freq = 4L, console = TRUE, agent.lr.decay = 1, agent.lr = 0.00025, replay.memname = "UniformStack", render = render, policy.decay = exp(-2.2 / 1e6), policy.minEpsilon = 0.1, agent.start.learn = 50000L, replay.mem.size = 1e6, log = FALSE)
   env = makeGymEnv("Pong-v0", act_cheat = c(2, 3), repeat_n_act = 4, observ_stack_len = 2L)
   agent = makeAgent("AgentFDQN", env, conf)
   perf = agent$learn(iter)
