8478a1876d0e233a44f539c4019fc64511ea9501
smilesun <smilesun.east@gmail.com>
2018-07-05 08:21:00 +0200

    .

--- tests/testthat/test_file_replay_mem.R
+++ tests/testthat/test_file_replay_mem.R
@@ -1,7 +1,10 @@
 context("replay_mem")
 test_that("test basic replay_mem works", {
+  conf = rlR.conf.DQN()
   mem = ReplayMem$new(agent, conf)
   mem$reset()
+  env = makeGymEnv("CartPole-v0")
+  agent = makeAgent("AgentFDQN", env, conf)
   ins = mem$mkInst(state.old = array(3), action = 1, reward = 1, state.new = array(4), done = TRUE, info = list())
   mem$add(ins)
   expect_class(mem, "ReplayMem")
@@ -14,6 +17,7 @@ test_that("test replay_mem works", {
   env = makeGymEnv("Pong-v0", repeat_n_act = 400, observ_stack_len = 2)
   env$overview()
   agent = makeAgent("AgentFDQN", env, conf)
+  expect_class(agent, "AgentFDQN")
 })
 
 context("interact")
