5bb162a1ce14d0c9a794a7d750aac1f85ea6837b
smilesun <smilesun.east@gmail.com>
2018-11-28 06:08:02 +0100

    repeated evaluate

--- R/experiment.R
+++ R/experiment.R
@@ -15,18 +15,43 @@ makeGymEnv = function(name ="CartPole-v0", ...) {
 }
 
 
-##' @title Make Gym Open AI experiment
-##'
-##' @description Make Gym Open AI experiment
-##'
-##' @param sname The scenario name of Gym environment
-##' @param aname The name of the Agent
-##' @param conf Configuration object
-##' @param ... Other Parameters to pass to GymEnv
-##' @return Interaction object
-##' @export
-#makeGymExperiment = function(sname ="CartPole-v0", aname, conf = NULL,  ...) {
-#  env = makeGymEnv(sname, ...)
-#  rl.agent = makeAgent(aname, env, conf = conf)
-#  return(rl.agent$interact)
-#}
+#' @title Repeat experiment
+#'
+#' @description Repeat the experiment for serveral times
+#'
+#' @param sname The scenario name of Gym environment
+#' @param aname The name of the Agent
+#' @param conf Configuration object
+#' @param nrep Number of repetitions
+#' @param nepi Number of episode to learn
+#' @param ... Other Parameters to pass to GymEnv
+#' @return ggplot2 object
+#' @export
+# repExperiment(aname = "AgentFDQN", conf = getDefaultConf("AgentFDQN"), nepi = 100)
+repExperiment = function(sname ="CartPole-v0", aname, conf, nrep = 3L, nepi, ...) {
+  library(foreach)
+  list.res = foreach::foreach(i = 1:nrep) %do% {
+    #env = makeGymEnv(sname, ...)
+    env = makeGymEnv(sname)
+    rl.agent = makeAgent(aname, env, conf = conf)
+    perf = rl.agent$learn(nepi)
+    list(agent = rl.agent, perf = perf)
+  }
+  list.r = lapply(list.res, function(res) {
+    res$perf$list.reward.epi})
+  init = list.r[[1L]]
+  agent = list.res[[1L]]$agent
+  len = agent$env$maxStepPerEpisode
+  convert2SameLen = function(init1) {
+    init2 = vector(mode = "numeric", length = len)
+    init2[1:length(init1)] = init1
+    init2
+  }
+  for (i in 2:length(list.r)) {
+    init = lapply(1:length(init), function(j) convert2SameLen(init[[j]]) + convert2SameLen(list.r[[i]][[j]]))
+  }
+  init = lapply(init, function(vec) vec/nrep)
+  agent$interact$perf$list.reward.epi = init
+  plot = agent$plotPerf()
+  return(plot)
+}
